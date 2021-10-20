6afb33fb4fc152d379ce4086dedc0496c42a8cb5
Peter Konnerup Moss <peter@moss.dk>
2021-10-20 14:44:01 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import { inspect } from 'util'
+
+enum TermColors {
+  reset = '\x1b[0m',
+  error = '\x1b[1;31m', // red
+  warning = '\x1b[1;33m', // yellow
+  info = '\x1b[1;34m', // blue
+  debug = '\x1b[1;36m', // cyan
+  verbose = '\x1b[34m', // green
+  developer = '\x1b[35m', // magenta
+}
+
+const generateLogString = (color: TermColors, msg: any) => {
+  return `${color}${msg}${TermColors.reset}`
+}
+
+const getDebugLevel = (debug?: string): number => {
+  if (!debug) return 0
+  const debugLC = debug.toLowerCase()
+  if (debug === '1' || debugLC === 'warn') return 1 // Turn on warn()
+  if (debug === '2' || debugLC === 'info') return 2 // Turn on info() and warn()
+  if (debug === '3' || debugLC === 'debug') return 3 // Turn on info() and warn()
+  if (debug === '4' || debugLC === 'verbose') return 4 // Turn on debug(), info() and warn()
+  if (debug === '5' || debugLC === 'developer') return 5 // Turn on debug(), info() and warn()
+  return 0 // Only err()
+}
+
+const DEBUG = getDebugLevel(process.env.DEBUG)
+
+export const warn = (msg: any, ...optionalParams: any[]) => {
+  if (DEBUG > 0) {
+    console.warn(generateLogString(TermColors.warning, '[Warning]'), msg, ...optionalParams)
+    return true
+  }
+  return false
+}
+
+export const info = (msg: any, ...optionalParams: any[]) => {
+  if (DEBUG > 1) {
+    console.info(generateLogString(TermColors.info, '[Info]'), msg, ...optionalParams)
+    return true
+  }
+  return false
+}
+
+export const debug = (msg: any, ...optionalParams: any[]) => {
+  if (DEBUG > 2) {
+    console.debug(generateLogString(TermColors.debug, '[Debug]'), msg, ...optionalParams)
+    return true
+  }
+  return false
+}
+
+export const verbose = (msg: any, ...optionalParams: any[]) => {
+  if (DEBUG > 3) {
+    console.debug(generateLogString(TermColors.verbose, '[Verbose]'), msg, ...optionalParams)
+    return true
+  }
+  return false
+}
+
+export const full = (msg: any, ...optionalParams: any[]) => {
+  if (DEBUG > 4) {
+    console.debug(generateLogString(TermColors.developer, '[Full]'), msg, ...optionalParams)
+    return true
+  }
+  return false
+}
+
+export const err = (msg: any, ...optionalParams: any[]) => {
+  console.error(generateLogString(TermColors.error, '[Critical]'), msg, ...optionalParams)
+  return true
+}
+
+export function trace(msg: any, error: Error, ...optionalParams: any[]): void;
+export function trace(msg: any, ...optionalParams: any[]): void;
+
+export function trace(msg: any, error?: Error, ...optionalParams: any[]): void {
+  console.error(generateLogString(TermColors.error, '[Critical]'), msg, ...optionalParams)
+  if (error)
+    console.error(generateLogString(TermColors.error, '[Trace]'), error.stack)
+  else
+    console.error(generateLogString(TermColors.error, '[Trace]'), new Error().stack)
+}
+
+export const fullObjString = (node: any) => {
+  return inspect(node, false, null, true)
+}
